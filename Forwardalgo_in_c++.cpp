154c1ec2118ab5639f049599dbd73f3178a07583
Pookpash <co_su-wobbe@web.de>
2016-12-01 10:50:54 +0100

    Update Forwardalgo_in_c++.cpp

--- Forwardalgo_in_c++.cpp
+++ Forwardalgo_in_c++.cpp
@@ -6,28 +6,27 @@ using namespace std;
 
 
 // [[Rcpp::export]]
-double forwardalgo(arma::rowvec x, arma::mat phi, arma::mat Gamma, arma::mat u, double l,
-                   arma::rowvec shape, arma::rowvec scale,int nrows) {
-        for(int t=2;t<nrows;t++) {
-                u = phi*Gamma;
-                arma::mat probvec = R::diag(dgamma(x[t],shape=shape,scale=scale,0));
-                u = u * probvec;
-                l = l+log(sum(u));
-                phi = u/sum(u);
+double forwardalgo(arma::rowvec foo, arma::mat gamma, arma::mat allprobs, double lscale,int nrows) {
+        for(int i=0;i<nrows;i++) {
+                foo = foo*gamma%allprobs.row(i);
+                double sumfoo = sum(foo);
+                lscale = lscale+log(sumfoo);
+                foo = foo/sumfoo;
         }
-        return l;
+        return lscale;
 }
 
 
 /*** R
-x <- c(24.98643, 23.42482, 22.25363, 24.98658, 28.93724, 33.67242, 34.72468, 28.47550, 35.85791, 33.74849,
-       35.77754, 12.02943, 14.94568, 21.50725, 21.50729, 41.63528, 42.27572, 30.74591, 20.49724, 43.89852)
-phi <- matrix(c(7.341532e-05,0.9999266),nrow=1)
-u <- matrix(c(7.607699e-07, 0.01036179),nrow=1)
-Gamma <- matrix(c(0.7091270, 0.2908730, 0.2825269, 0.7174731),nrow=2,byrow=T)
-l <- -4.569557
-mu <- c(117.53181, 38.37499)
-sig <- c(31.56159, 23.46791)
-result <- forwardalgo(x,phi,Gamma,u,l,scale = mu^2/sig^2,shape = sig^2/mu,20))
+foo <- c(0.8,0.2)
+allprobs <- matrix(c(0.041187346, 4.840003e-04, 0.039264483, 3.077633e-04, 0.037015958, 2.121437e-04, 0.041187472,
+                     4.840205e-04, 0.040742014, 1.264151e-03, 0.033085791, 3.036417e-03, 0.030864256, 3.570524e-03,
+                     0.041144891, 1.143672e-03, 0.028404224, 4.202477e-03, 0.032928408, 3.073300e-03, 0.028579731, 
+                     4.155762e-03, 0.005797453, 1.253671e-06, 0.013722984, 8.651436e-06, 0.035245663, 1.646933e-04,
+                     0.035245781, 1.646960e-04, 0.016583294, 8.208760e-03, 0.015461981, 8.713352e-03, 0.038462354, 
+                     1.822016e-03, 0.032477476, 1.144015e-04, 0.012844118, 1.002045e-02),nrow=20,byrow=T)
+gamma <- matrix(c(0.8,0.2,0.8,0.2),nrow=2,byrow=T)
+lscale <- 0
+result <- forwardalgo(foo,gamma,allprobs,lscale,20)
 result
 */
